295703baf0823f56e5c26252eaabe3c7929c1637
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-17 22:15:11 -0500

    add  Category-rotes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,51 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  //https://sequelize.org/master/class/lib/associations/base.js~Association.html
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  //https://sequelize.org/master/manual/model-querying-finders.html
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No Category found with this id!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 router.post('/', (req, res) => {

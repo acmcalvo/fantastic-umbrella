bf2d3a6c9bb0676a307bf93bb1c9554ee84d8ffd
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-21 20:34:55 -0500

    add on index.js belongs hasmay and belogsToMany

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+ });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+});
 
 module.exports = {
   Product,

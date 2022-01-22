0cf561fedc3c08109e11d3613cbe67d7e83f8b9b
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-21 21:02:24 -0500

    add update and delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,10 +68,39 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(updateCategory => {
+    if(!updateCategory) {
+      res.status(404).json({message: `No category found with ID ${id}`});
+      return;
+    }
+    res.json(updateCategory);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+}); 
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({message: `No category found with ID ${id}`});
+      return;
+    }
+    res.json(dbCategoryData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
 
 module.exports = router;

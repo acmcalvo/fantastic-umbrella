4255c786edd5cc2a3ce5adbb56b97916bfd6d84d
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-22 14:46:02 -0500

    updates  routes

--- models/Product.js
+++ models/Product.js
@@ -16,44 +16,45 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
 
-  },
+  },        
+  // define product_name column
   product_name: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false
   },
-
+  // define price column
   price: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    validate: {
-        isDecimal: true
-    }
-},
-
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+          isDecimal: true
+      }
+  },
+  // define stock column
   stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 10,
-    validate: {
-      isNumeric: true,
-    },
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+          isNumeric: true
+      }
   },
+  // define category_id column
   category_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'category',
-      key: 'id',
-    },
-  },
-},
-
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+      type: DataTypes.INTEGER,
+      references: {
+          model: "category",
+          key: "id"
+      }
   }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+}
 );
 
 module.exports = Product;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,18 +10,19 @@ router.get('/', (req, res) => {
   Category.findAll({
     attributes: ['id', 'category_name'],
     include: [
-      {
-        model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock'],
-      },
-    ],
-  })
-    .then((dbCategoryData) => res.json(dbCategoryData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]
+})
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
@@ -29,40 +30,38 @@ router.get('/:id', (req, res) => {
   //https://sequelize.org/master/manual/model-querying-finders.html
   Category.findOne({
     where: {
-      id: req.params.id,
+        id: req.params.id
     },
     attributes: ['id', 'category_name'],
     include: [
-      {
-        model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock'],
-      },
-    ],
-  })
-    .then((dbCategoryData) => {
-      if (!dbCategoryData) {
-        res.status(404).json({ message: 'No Category found with this id!' });
-        return;
-      }
-      res.json(dbCategoryData);
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]
+})
+    .then(dbCategoryData => {
+        if (!dbCategoryData) {
+            res.status(404).json({ message: 'No category found with this id' });
+            return;
+        }
+        res.json(dbCategoryData);
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
-
 });
 
 router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    id: req.body.id,
-    category_name: req.body.category_name,
-  })
-    .then((dbCategoryData) => res.json(dbCategoryData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+    category_name: req.body.category_name
+})
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
@@ -70,37 +69,40 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
     where: {
-      id: req.params.id
+        id: req.params.id
     }
-  }).then(updateCategory => {
-    if(!updateCategory) {
-      res.status(404).json({message: `No category found with ID ${id}`});
-      return;
-    }
-    res.json(updateCategory);
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-}); 
+})
+    .then(dbCategoryData => {
+        if (!dbCategoryData[0]) {
+            res.status(404).json({ message: 'No Category found with this id' });
+            return;
+        }
+        res.json(dbCategoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
-    where: {
-      id: req.params.id
-    }
-  }).then(dbCategoryData => {
-    if(!dbCategoryData) {
-      res.status(404).json({message: `No category found with ID ${id}`});
-      return;
-    }
-    res.json(dbCategoryData);
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+      where: {
+          id: req.params.id
+      }
+  })
+      .then(dbCategoryData => {
+          if (!dbCategoryData) {
+              res.status(404).json({ message: 'No category found with this id' });
+              return;
+          }
+          res.json(dbCategoryData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
-
 
 module.exports = router;
